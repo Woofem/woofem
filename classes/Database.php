9f4ba87d2568731c0a133481e4416e27df28fa76
Thomas Britton <tom.britton@wholefoods.com>
2015-10-15 16:40:20 -0500

    Added comments

--- classes/Database.php
+++ classes/Database.php
@@ -1,11 +1,20 @@
 <?php
 
+/**
+ * @file Database connection class.
+ */
+
 namespace Woofem;
 
 class Database {
 
     public $connection;
 
+    /**
+     * Constructor function.
+     * @param $config object
+     *   Application database configuration object.
+     */
     public function __construct($config) {
         $this->connection = new \PDO(
             'mysql:host=' . $config->db_host . ';dbname=' . $config->db_name,
